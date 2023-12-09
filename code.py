730cc4549e11d836546291e0d8d659182ca33a04
silaswilson87 <123284141+silaswilson87@users.noreply.github.com>
2023-12-08 17:40:16 -0800

    Update code.py
    Clean up obsolete items and tidy remarks.

--- code.py
+++ code.py
@@ -1,35 +1,31 @@
 import board
 import digitalio
 import time
-from digitalio import DigitalInOut, Direction, Pull
+from digitalio import DigitalInOut, Direction,
 
-mosfet = digitalio.DigitalInOut(board.D13)  # mosfset switch controlling a 12V Pnuematic solenoid valve between digital pin12 and ground
-mosfet.direction = digitalio.Direction.OUTPUT  # Output directed control signal
+mosfet = digitalio.DigitalInOut(board.D13)               # Mosfset switch controlling a 12V Pnuematic solenoid valve between digital pin12 and ground
+mosfet.direction = digitalio.Direction.OUTPUT            
 
-# status lights #
-red = digitalio.DigitalInOut(board.D12)  # Automatic state
+red = digitalio.DigitalInOut(board.D12)                  # Automatic state
 red.direction = digitalio.Direction.OUTPUT
-green = digitalio.DigitalInOut(board.D11)  # Mosfet trigger light
+green = digitalio.DigitalInOut(board.D11)                # Mosfet trigger light
 green.direction = digitalio.Direction.OUTPUT
-blue = digitalio.DigitalInOut(board.D10)  # Manual state
+blue = digitalio.DigitalInOut(board.D10)                 # Manual state
 blue.direction = digitalio.Direction.OUTPUT
 
-touch_pin = DigitalInOut(board.D7)  # Push button
+touch_pin = DigitalInOut(board.D7)                       # Push button
 touch_pin.direction = digitalio.Direction.INPUT
-touch_pin.pull = Pull.DOWN
-touch_val = touch_pin.value  # Assign Boolean value of pin D7 to variable
+touch_val = touch_pin.value                              # Assign Boolean value of pin D7 to variable
 
-manual_pin = DigitalInOut(board.D2)  # Manual mode
+manual_pin = DigitalInOut(board.D2)                      # Manual mode
 manual_pin.direction = digitalio.Direction.INPUT
-manual_pin.pull = Pull.DOWN
-manual_pin_val = manual_pin.value  # Assign Boolean value of pin D2 to variable
+manual_pin_val = manual_pin.value                        # Assign Boolean value of pin D2 to variable
 
-auto_pin = DigitalInOut(board.D1)  # Automatic mode
+auto_pin = DigitalInOut(board.D1)                        # Automatic mode
 auto_pin.direction = digitalio.Direction.INPUT
-auto_pin.pull = Pull.DOWN
-auto_pin_val = auto_pin.value  # Assign Boolean value of pin D1 to variable
+auto_pin_val = auto_pin.value                            # Assign Boolean value of pin D1 to variable
 
-def manual():  # Logic for manual mode of button
+def manual():                                            # Controller for manual mode of button
         print("-Manual- Initiate valve")
         green.value = True
         mosfet.value = True
@@ -37,22 +33,14 @@ def manual():  # Logic for manual mode of button
         green.value = False
         mosfet.value = False
         
-def auto():  # Logic for button
+def auto():                                              # Control for manual mode of button
         print("-Auto- Initiate valve")
         green.value = True
         mosfet.value = True
-        time.sleep(.6)
+        time.sleep(.5)
         green.value = False
         mosfet.value = False
-        time.sleep(.6)
-
-def manual_button():  # Manual Button logic initializer
-    if str(touch_val) == "True":
-        manual()
-
-def auto_button():  # Automatic Button logic initializer
-    if str(touch_val) == "True":
-        auto()
+        time.sleep(.5)
 
 print("(-Base-level 0)")
 print("Current slide switch Manual orientation = " + str(manual_pin_val))
@@ -63,13 +51,17 @@ while True:
     touch_val = touch_pin.value
     manual_pin_val = manual_pin.value
     auto_pin_val = auto_pin.value
-    if manual_pin.value :  # Calls manual fuction of push button mosfet control
+    if manual_pin.value :                               # Calls manual function of push button mosfet control
         blue.value = True
         red.value = False
         if manual_pin.value and touch_pin.value:
             manual()            
-    elif auto_pin.value :  # Calls auto fuction of push button mosfet control
+    elif auto_pin.value :                               # Calls auto function of push button mosfet control
         red.value = True
         blue.value = False
         if auto_pin.value and touch_pin.value:
             auto()
+
+    
+
+
